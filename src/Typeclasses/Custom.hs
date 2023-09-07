28deee729dc45b9216dafb1d2b929a93a8552c7b
ja-cha <jan_abt@yahoo.com>
2023-09-07 21:27:53 +0700

    Option Functor

--- src/Typeclasses/Custom.hs
+++ src/Typeclasses/Custom.hs
@@ -1,33 +1,55 @@
-module Typeclasses.Custom (CustomEq(..), TrafficLight(..)) where
+module Typeclasses.Custom
+  ( CustomEq(..)
+  , TrafficLight(..)
+  , Option(..)
+  ) where
+
 
 -- defining a new typeclass  called CustomEq
 -- "equatable" is the type variable, a stand-in for the type that we will soon be making an instance of
-class CustomEq equatable where
+class CustomEq equatable
+ where
   (===) :: equatable -> equatable -> Bool
   (/==) :: equatable -> equatable -> Bool
-
   x === y = not (x /== y)
   x /== y = not (x === y)
 
+
 -- we want all types of the form Maybe m to be part of the CustomEq typeclass
 -- where what's contained inside the Maybe is also a part of CustomEq
-instance (CustomEq m) => CustomEq (Maybe m) where  
-    Just x === Just y = x === y  
-    Nothing === Nothing = True  
-    _ === _ = False  
+instance (CustomEq m) => CustomEq (Maybe m)
+ where
+  Just x === Just y   = x === y
+  Nothing === Nothing = True
+  _ === _             = False
 
-data TrafficLight = Red | Yellow | Green  
+data TrafficLight
+  = Red
+  | Yellow
+  | Green
 
-instance CustomEq TrafficLight where  
-    Red === Red = True  
-    Green === Green = True  
-    Yellow === Yellow = True  
-    _ === _ = False  
+instance CustomEq TrafficLight
+ where
+  Red === Red       = True
+  Green === Green   = True
+  Yellow === Yellow = True
+  _ === _           = False
 
-instance Show TrafficLight where  
-    show Red = "Red light"  
-    show Yellow = "Yellow light"  
-    show Green = "Green light" 
+instance Show TrafficLight
+ where
+  show Red    = "Red light"
+  show Yellow = "Yellow light"
+  show Green  = "Green light"
 
+data Option a
+  = Some a
+  | None
+  deriving (Show, Read, Eq)
 
-        
+instance Functor Option
+ where
+  fmap = optionMap
+
+optionMap :: (a -> b) -> Option a -> Option b
+optionMap f None     = None
+optionMap f (Some x) = Some (f x)
--- test/Typeclasses/CustomSpec.hs
+++ test/Typeclasses/CustomSpec.hs
@@ -1,5 +1,5 @@
 import           Test.Hspec
-import           Typeclasses.Custom (CustomEq(..),TrafficLight(..))
+import           Typeclasses.Custom (CustomEq(..),TrafficLight(..),  Option(..))
 
 main :: IO ()
 main =
@@ -8,5 +8,16 @@ main =
       it "Red === Red `shouldBe` True" $ do
         Red === Red  `shouldBe` True
       it "Red /== Green `shouldBe` True" $ do
-        Red /== Green  `shouldBe` True
+        Red /== Green  `shouldBe` True     
+    describe ">>>>>> Custom Option <<<<<<" $ do    
+      it "Some(1) == Some(1)  `shouldBe` True" $ do
+        Some(1) == Some(1)  `shouldBe` True     
+      it "fmap (\"Jan \" ++) (Some \"Abt\")  `shouldBe` Some(\"Jan Abt\" )" $ do
+        fmap ("Jan " ++) (Some "Abt")  `shouldBe` Some("Jan Abt")     
+      it "fmap (+2) (Some 1)  `shouldBe` Some(3)" $ do
+        fmap (+2) (Some 1)  `shouldBe` Some(3)     
+      it "fmap (+2) (None)  `shouldBe` None" $ do
+        fmap (+2)  None  `shouldBe` None     
+      it "fmap (1:) (Some [2])  `shouldBe` Some [1,2]" $ do
+        fmap (1:) (Some [2])  `shouldBe` Some [1,2]
      
